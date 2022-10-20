750371cdfa303b466788fd4e8d3f0e37d8b22268
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-20 12:25:03 +0530

    made improvements

--- src/contracts/hangout.sol
+++ src/contracts/hangout.sol
@@ -2,7 +2,7 @@
  
 pragma solidity >=0.7.0 <0.9.0; 
  
- 
+import "@openzeppelin/contracts/utils/Counters.sol";
 interface IERC20Token { 
   function transfer(address, uint256) external returns (bool); 
   function approve(address, uint256) external returns (bool); 
@@ -19,8 +19,10 @@ interface IERC20Token {
  
 contract Coupleshangout { 
  
-    uint internal hangoutLength = 0; 
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1; 
+    using Counters for Counters.Counter;
+    Counters.Counter hangoutLength;
+
+    address internal constant cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1; 
  
  
     // declaring the struct for the review 
@@ -44,32 +46,66 @@ contract Coupleshangout {
  
     mapping(uint256=> Hangout) internal hangouts; 
     mapping (uint => Review[]) internal reviewsMap;// mapping reviews 
+    mapping (uint => mapping(address => bool)) internal hasBought;
  
- 
+    modifier validString(string memory input){
+        require(bytes(input).length > 0, "invalid input");
+        _;
+    }
     function addHangout( 
         string memory _country, 
         string memory _image, 
         string memory _description, 
         string memory _location, 
         uint _price 
-    )public{ 
-        uint _sold = 0; 
-        uint _numberOfReview = 0; 
- 
- 
-        hangouts[hangoutLength] = Hangout( 
+    )   public
+        validString(_country) validString(_image)
+        validString(_description) validString(_location)
+    {  
+        require(_price > 0, "invalid input");
+        hangouts[hangoutLength.current()] = Hangout( 
             payable(msg.sender), 
             _country, 
             _image, 
             _description, 
             _location, 
             _price, 
-            _sold, 
-            _numberOfReview 
+            0, 
+            0 
         ); 
-        hangoutLength++; 
+        hangoutLength.increment(); 
     } 
  
+    function bookHangout(uint _index) public payable  {
+        require(msg.sender != hangouts[_index].owner, "owner can't buy");
+        require( 
+          IERC20Token(cUsdTokenAddress).transferFrom( 
+            msg.sender, 
+            hangouts[_index].owner, 
+            hangouts[_index].price 
+          ), 
+          "Can not perform transactions." 
+        ); 
+        hasBought[_index][msg.sender] = true;
+    } 
+ 
+    // add a review to a vacation couples hangout 
+   function addReview(uint _index, string memory _reviews) public{
+        require(hasBought[_index][msg.sender] == true, "only buyers can review");
+        reviewsMap[_index].push(Review(_index, address(msg.sender), _reviews)); 
+        hangouts[_index].numberOfReview++; 
+    } 
+ 
+ 
+    function getHangoutLength() public view returns(uint){ 
+        return hangoutLength.current(); 
+    } 
+ 
+    // acquiring length of reviews  
+    function getReviewLength(uint _index) public view returns (uint) { 
+        return reviewsMap[_index].length; 
+    } 
+
     function getHangout(uint _index)public view returns( 
         address payable, 
         string memory, 
@@ -94,34 +130,5 @@ contract Coupleshangout {
             h.numberOfReview, 
             reviews 
         ); 
-    } 
- 
- 
- 
-       // add a review to a vacation couples hangout 
-   function addReview(uint _index, string memory _reviews) public{ 
-    reviewsMap[_index].push(Review(_index, address(msg.sender), _reviews)); 
-    hangouts[_index].numberOfReview++; 
-  } 
- 
- 
-function bookHangout(uint _index) public payable  { 
-        require( 
-          IERC20Token(cUsdTokenAddress).transferFrom( 
-            msg.sender, 
-            hangouts[_index].owner, 
-            hangouts[_index].price 
-          ), 
-          "Can not perform transactions." 
-        ); 
-    } 
- 
-    function getHangoutLength() public view returns(uint){ 
-        return hangoutLength; 
-    } 
- 
-    // acquiring length of reviews  
-    function getReviewLength(uint _index) public view returns (uint) { 
-        return reviewsMap[_index].length; 
     } 
 }
